6b309c2162beaaf39c1fe7b6ff31be60bda024f0
Tommy Sugiarto <tommysugiarto@Tommys-MacBook-Air.local>
2019-04-18 16:47:22 +0700

    update 18.04.19 c

--- assets/js/style.js
+++ assets/js/style.js
@@ -571,19 +571,20 @@
 	}
 
 	let couponToggler = document.getElementsByClassName('js-coupon-toggler');
-	if(couponToggler[0]) {
-		couponToggler = couponToggler[0];
+	for(var i = 0; i < couponToggler.length; i++)
+	{
+		if(couponToggler[i]) {
+			couponToggler[i].onclick = function() {
+				let $display = this.parentElement.getElementsByClassName('js-coupon-display')[0];
 
-		couponToggler.onclick = function() {
-			let $display = this.parentElement.getElementsByClassName('js-coupon-display')[0];
-
-			if($display.classList.contains('is-hidden')) {
-				$display.classList.remove('is-hidden');
-			}
-			else {
-				$display.classList.add('is-hidden');
-			}
-		};
+				if($display.classList.contains('is-hidden')) {
+					$display.classList.remove('is-hidden');
+				}
+				else {
+					$display.classList.add('is-hidden');
+				}
+			};
+		}
 	}
 
 	// Run Functions
